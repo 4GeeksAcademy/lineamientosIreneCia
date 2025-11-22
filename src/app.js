66ac0ecaf6c5d575a3b21c57d621b13e9df85d6a
Irenecia <irenecia77@gmail.com>
2025-11-22 11:23:04 +0000

    cambio let por const

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 
-  
-  let pronouns = ['the', 'our'];
-  let adjs = ['great', 'big'];
-  let nouns = ['jogger', 'racoon'];
 
-  let domains = [];
-  
-  pronouns.forEach((pronoun) => {
-    adjs.forEach((adj) => {
-      nouns.forEach((noun) => {
-        domains.push(`${pronoun}${adj}${noun}.com`)
-      })
+const pronouns = ['the', 'our'];
+const adjs = ['great', 'big'];
+const nouns = ['jogger', 'racoon'];
+
+const domains = [];
+
+pronouns.forEach((pronoun) => {
+  adjs.forEach((adj) => {
+    nouns.forEach((noun) => {
+      domains.push(`${pronoun}${adj}${noun}.com`)
     })
   })
-    
-  
+})
 
-  
-  //write your code here
-  console.log(domains);
-  console.log("Hello Rigo from the console!");
+
+
+
+//write your code here
+console.log(domains);
+console.log("Hello Rigo from the console!");
 
